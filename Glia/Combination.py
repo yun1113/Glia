819ed4fd5e7b47fb88d375c4da46beeb39dc5c2c
yun1113 <yunyun1113c@gmail.com>
2016-05-25 23:33:02 +0800

    Glia_Project_ver3
    Glia_Project_ver3

--- Glia/Combination.py
+++ Glia/Combination.py
@@ -8,11 +8,38 @@ C(n,1) = n
 C(n,n) = 1
 Compute  C( 990, 33 )
 '''
+
+# VERY SLOW ...
+# def combination(n, r):
+#     if r == 1:
+#         return n
+#     if r == n:
+#         return 1
+#     return combination(n - 1, r) + combination(n - 1, r - 1)
+# 
+# print(combination(990,33))
+
+ 
+import time
+ 
+combDict = {}
+ 
 def combination(n, r):
+    if (n, r) in combDict.keys():
+        return combDict[(n, r)]
+     
     if r == 1:
         return n
     if r == n:
         return 1
-    return combination(n - 1, r) + combination(n - 1, r - 1)
-
-print(combination(5,2))
+     
+    combDict[(n - 1, r)] = combination(n - 1, r)
+    combDict[(n - 1, r - 1)] = combination(n - 1, r - 1)
+     
+    return  combDict[(n - 1, r)] + combDict[(n - 1, r - 1)] 
+ 
+# tStart = time.time()
+# print(combination(5, 2))
+print(combination(990,33))
+# tEnd = time.time()
+# print("cost: ", tStart - tEnd)
